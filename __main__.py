12974e62c3283a67f5eb9236e47febd6f9f85c10
Oliver Ju <n@o.ne>
2020-03-29 16:43:47 +0200

    Merge branch 'feature/mpld3_hover_data'

--- __main__.py
+++ __main__.py
@@ -36,6 +36,7 @@ html_template = """
 <html>
 <head>
 <title>Corona Statistics</title>
+<meta charset="UTF-8">
 </head>
 <body>
 <table style="width:100%">
@@ -53,8 +54,9 @@ def process_country(country, data_dict):
     dates_dict = {}
 
     # filter data
+    regex = re.compile('\d+/\d+/\d+')
     for k, v in data_dict.iteritems():
-        if (re.match('\d+/\d+/\d+', k) and float(v) > treshold):
+        if (regex.match(k) and float(v) > treshold):
             date_time = datetime.strptime(k, '%m/%d/%y')
             dates_dict[date_time] = float(v)
 
@@ -67,17 +69,31 @@ def process_country(country, data_dict):
 
     # calculate doubling rate
     rate = numpy.divide(number_of_deseases[1:], number_of_deseases[:-1])
+
+    for i in range(len(rate)):
+        if rate[i] == 1.0:
+            rate[i] = 100000000  # force doubling rate to be 0
+
     log2_vector = numpy.full((len(rate)), numpy.log(2))
     doubling_rate = [0.0] + list(numpy.divide(log2_vector, numpy.log(rate)))
 
     labelx = -0.12
 
+    def generate_tool_tips(points):
+        captions = ["<h4 style=\"background-color:powderblue;\">{:0.1f} ({})</h4>".format(val, val_date.strftime("%d.%m.%Y")) for (val_date, val) in zip(points[0].get_data()[0], points[0].get_data()[1])]
+        mpld3.plugins.connect(plt.gcf(), mpld3.plugins.PointHTMLTooltip(points[0], labels=captions))
+        pass
+
+
+    # this is important otherwise plots will have relation to each other and null exceptions occur during printing of the second graph
+    plt.figure()
+
     # total number of deseases
     ax1 = plt.subplot(411)
     plt.title(country)
-    plt.ylabel("Number of corona")
+    plt.ylabel("No.")
     plt.grid(True)
-    plt.plot(d, number_of_deseases)
+    generate_tool_tips(plt.plot(d, number_of_deseases, "s-"))
     plt.gcf().autofmt_xdate()
     ax1.yaxis.set_label_coords(labelx, 0.5)
 
@@ -85,32 +101,31 @@ def process_country(country, data_dict):
     ax2 = plt.subplot(412, sharex=ax1)
     plt.grid(True)
     plt.yscale("log")
-    plt.plot(d, number_of_deseases)
+    generate_tool_tips(plt.plot(d, number_of_deseases, "s-"))
     plt.gcf().autofmt_xdate()
     ax2.yaxis.set_label_coords(labelx, 0.5)
 
     # new deseases
     ax3 = plt.subplot(413, sharex=ax1)
-    plt.ylabel("Delta")
+    plt.ylabel("delta")
     plt.grid(True)
-    plt.bar(d, number_of_new_deseases, width=0.4)
+    generate_tool_tips(plt.plot(d, number_of_new_deseases, "s-"))
     plt.gcf().autofmt_xdate()
     ax3.yaxis.set_label_coords(labelx, 0.5)
 
     # doubling rate
     ax4 = plt.subplot(414, sharex=ax1)
-    plt.ylabel("Doubl.-Rate")
+    plt.ylabel("Dbl.-Rate")
     plt.grid(True)
-    plt.bar(d, doubling_rate, width=0.4)
+    generate_tool_tips(plt.plot(d, doubling_rate, "s-"))
     plt.gcf().autofmt_xdate()
     ax4.yaxis.set_label_coords(labelx, 0.5)
-
     result = mpld3.fig_to_html(plt.gcf())
-    plt.clf()
+
     return result
 
 
-if __name__ == "__main__":
+def main():
     file_name = "data.csv"
     print "get {}".format(file_name)
     urllib.urlretrieve(csv_source, filename=file_name)
@@ -143,7 +158,11 @@ if __name__ == "__main__":
                 used_template = table_row_single_template
             table_rows = table_rows + Template(used_template).substitute(**locals()) + "\n"
 
-        html_content = Template(html_template).substitute(**locals())
+        html_content = Template(html_template).substitute(csv_source=csv_source, **locals())
         open("index.html", "w").write(html_content)
 
     print "done"
+
+
+if __name__ == "__main__":
+    main()
