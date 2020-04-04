65cbd6a7bcc9fc8d032f2d6af4c0c5e5f8a07298
Oliver Ju <n@o.ne>
2020-04-04 09:25:19 +0200

    Python3 and use StringIO for csvfile

--- __main__.py
+++ __main__.py
@@ -1,12 +1,15 @@
-import csv
-import collections
-import numpy
-import re
-from datetime import datetime
-from matplotlib import pyplot as plt
 import mpld3
-import urllib
+import numpy as np
+import re
+import sys
+
+from collections import OrderedDict
+from csv import DictReader
+from datetime import datetime
+from io import StringIO
+from matplotlib import pyplot as plt
 from string import Template
+from urllib.request import urlopen
 
 csv_source = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv"
 treshold = 100
@@ -50,32 +53,32 @@ $table_rows
 
 
 def process_country(country, data_dict):
-    print "process country {}".format(country)
+    print ("process country {}".format(country))
     dates_dict = {}
 
     # filter data
     regex = re.compile('\d+/\d+/\d+')
-    for k, v in data_dict.iteritems():
+    for k, v in data_dict.items():
         if (regex.match(k) and float(v) > treshold):
             date_time = datetime.strptime(k, '%m/%d/%y')
             dates_dict[date_time] = float(v)
 
     # sort data
-    dates_dict = collections.OrderedDict(sorted(dates_dict.items()))
+    dates_dict = OrderedDict(sorted(dates_dict.items()))
 
     lists = sorted(dates_dict.items())
     d, number_of_deseases = zip(*lists)
-    number_of_new_deseases = [0] + list(numpy.diff(number_of_deseases))
+    number_of_new_deseases = [0] + list(np.diff(number_of_deseases))
 
     # calculate doubling rate
-    rate = numpy.divide(number_of_deseases[1:], number_of_deseases[:-1])
+    rate = np.divide(number_of_deseases[1:], number_of_deseases[:-1])
 
     for i in range(len(rate)):
         if rate[i] == 1.0:
             rate[i] = 100000000  # force doubling rate to be 0
 
-    log2_vector = numpy.full((len(rate)), numpy.log(2))
-    doubling_rate = [0.0] + list(numpy.divide(log2_vector, numpy.log(rate)))
+    log2_vector = np.full((len(rate)), np.log(2))
+    doubling_rate = [0.0] + list(np.divide(log2_vector, np.log(rate)))
 
     labelx = -0.12
 
@@ -83,7 +86,6 @@ def process_country(country, data_dict):
         captions = ["<h4 style=\"background-color:powderblue;\">{:0.1f} ({})</h4>".format(val, val_date.strftime("%d.%m.%Y")) for (val_date, val) in zip(points[0].get_data()[0], points[0].get_data()[1])]
         mpld3.plugins.connect(plt.gcf(), mpld3.plugins.PointHTMLTooltip(points[0], labels=captions))
         pass
-
 
     # this is important otherwise plots will have relation to each other and null exceptions occur during printing of the second graph
     plt.figure()
@@ -127,41 +129,44 @@ def process_country(country, data_dict):
 
 def main():
     file_name = "data.csv"
-    print "get {}".format(file_name)
-    urllib.urlretrieve(csv_source, filename=file_name)
-    with open(file_name) as csvfile:
-        dict_reader = csv.DictReader(csvfile, delimiter=',')
-        countries = ['Austria', 'Italy', 'Germany', 'Spain', 'Turkey']
-        html_graphs = []
-        processed_countries = []
-        for data_dict in dict_reader:
-            current_country = data_dict['Country/Region']
-            if current_country in countries:
-                current_country_html = process_country(current_country, data_dict)
-                processed_countries.append(current_country)
-                html_graphs.append(current_country_html)
-        if sorted(countries) != sorted(processed_countries):
-            print "Error: could not process all countries. Not found: {}".format(list(set(countries) - set(processed_countries)))
+    print ("get {}".format(file_name))
+    filedata = urlopen(csv_source)
+    csvfile = StringIO(filedata.read().decode("utf8"))
+    dict_reader = DictReader(csvfile, delimiter=',')
+    if len(sys.argv) < 2:
+        countries = ['Austria', 'Italy', 'Germany', 'Spain', 'Turkey', 'Russia']
+    else:
+        countries = sys.argv[1:]
+    html_graphs = []
+    processed_countries = []
+    for data_dict in dict_reader:
+        current_country = data_dict['Country/Region']
+        if current_country in countries:
+            current_country_html = process_country(current_country, data_dict)
+            processed_countries.append(current_country)
+            html_graphs.append(current_country_html)
+    if sorted(countries) != sorted(processed_countries):
+        print ("Error: could not process all countries. Not found: {}".format(list(set(countries) - set(processed_countries))))
 
-        table_rows = ""
+    table_rows = ""
 
-        def chunks(l, n):
-            n = max(1, n)
-            return (l[i:i + n] for i in range(0, len(l), n))
+    def chunks(l, n):
+        n = max(1, n)
+        return (l[i:i + n] for i in range(0, len(l), n))
 
-        for chunk in chunks(html_graphs, 2):
-            plot_left = chunk[0]
-            plot_right = chunk[1] if len(chunk) > 1 else ""
-            if len(chunk) > 1:
-                used_template = table_row_template
-            else:
-                used_template = table_row_single_template
-            table_rows = table_rows + Template(used_template).substitute(**locals()) + "\n"
+    for chunk in chunks(html_graphs, 2):
+        plot_left = chunk[0]
+        plot_right = chunk[1] if len(chunk) > 1 else ""
+        if len(chunk) > 1:
+            used_template = table_row_template
+        else:
+            used_template = table_row_single_template
+        table_rows = table_rows + Template(used_template).substitute(**locals()) + "\n"
 
-        html_content = Template(html_template).substitute(csv_source=csv_source, **locals())
-        open("index.html", "w").write(html_content)
+    html_content = Template(html_template).substitute(csv_source=csv_source, **locals())
+    open("index.html", "w").write(html_content)
 
-    print "done"
+    print ("done")
 
 
 if __name__ == "__main__":
